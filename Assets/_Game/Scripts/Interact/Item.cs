f47dada5c9e3502e9063967aacad658ef1a1b5dc
TauPhika <berninimatias03@gmail.com>
2026-03-29 20:15:22 -0300

    armando el menu de objectos

--- Assets/_Game/Scripts/Interact/Item.cs
+++ Assets/_Game/Scripts/Interact/Item.cs
@@ -1,9 +1,17 @@
 using Sirenix.OdinInspector;
+using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(menuName = "Game/Item",fileName = "Item")]
 public class Item : ScriptableObject
 {
     public GameObject gameObject;
+    [SerializeReference] public Clue clueInfo;
 
     [MinValue(1)] public float size;
+}
+
+public class Clue
+{
+    [TextArea(0,20)] public List<string> clues;
+    public GameObject clueDisplay;
 }
