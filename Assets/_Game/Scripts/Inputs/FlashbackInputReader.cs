f9ec9a4666dd27c316375c505610bb749e75da56
TauPhika <berninimatias03@gmail.com>
2026-04-16 13:38:41 -0300

    bug boludo solucionado

--- Assets/_Game/Scripts/Inputs/FlashbackInputReader.cs
+++ Assets/_Game/Scripts/Inputs/FlashbackInputReader.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "Flashback Input", menuName = "Game/InputReader/Flashback")]
-public class FlashbackInputReader : CCInputReader, InputActions.IFlashbackActions
+public class FlashbackInputReader : InputReader, InputActions.IFlashbackActions
 {
     public event Action ExitFlashback = delegate { };
     public void OnExit(InputAction.CallbackContext context)
@@ -11,4 +11,14 @@ public class FlashbackInputReader : CCInputReader, InputActions.IFlashbackAction
         if(context.started) ExitFlashback?.Invoke();
     }
 
+    public override void SetEnable(bool enable = true)
+    {
+        if (enable) InputAction?.Flashback.Enable();
+        else InputAction?.Flashback.Disable();
+    }
+
+    protected override void SetCallback(InputActions inputAction)
+    {
+        inputAction?.Flashback.SetCallbacks(this);
+    }
 }
