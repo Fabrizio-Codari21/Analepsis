5f1ddf60d7c316a65e9aca7670407f20043006fe
TauPhika <berninimatias03@gmail.com>
2026-05-03 15:27:46 -0300

    mejoras en las particulas

--- Assets/_Game/Scripts/Touch/TouchParticle.cs
+++ Assets/_Game/Scripts/Touch/TouchParticle.cs
@@ -5,6 +5,7 @@ public class TouchParticle : MonoBehaviour
     private ITouch _owner;
     
     [SerializeField] private GameObject _particle;
+    [SerializeField] private ParticleSystem _particleSystem;
 
     private void Awake()
     {
@@ -18,7 +19,7 @@ public class TouchParticle : MonoBehaviour
     private void Start()
     {
         if(!_particle) return;
-        _particle.SetActive(false);
+        //_particle.SetActive(false);
         _owner.OnFocus += ActiveGameObject;
         _owner.OnUnfocus += DeactiveGameObject;
     }
@@ -26,11 +27,14 @@ public class TouchParticle : MonoBehaviour
 
     private void ActiveGameObject()
     {
-        _particle.SetActive(true);
+        //_particle.SetActive(true);
+        _particleSystem.Play();
+
     }
 
     private void DeactiveGameObject()
     {
-        _particle.SetActive(false);
+        //_particle.SetActive(false);
+        _particleSystem.Stop();
     }
 }
