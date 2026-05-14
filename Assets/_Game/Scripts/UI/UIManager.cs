e4c49dcfe9732d6e5be9083d91ce64dc057719bc
TauPhika <berninimatias03@gmail.com>
2026-05-14 17:52:25 -0300

    mini ajustes

--- Assets/_Game/Scripts/UI/UIManager.cs
+++ Assets/_Game/Scripts/UI/UIManager.cs
@@ -7,7 +7,7 @@ public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private GameObject[] m_uIObjects;
 
-    [InfoBox("Make sure that the bars' scale is not below 0.6")]
+    [InfoBox("NOTE: Make sure that the bars' scale is not below 0.6")]
     [SerializeField] private Transform m_aspectRatioBars;
 
     public float AspectRatioOffset(float max = 540f) =>
@@ -18,6 +18,7 @@ public class UIManager : Singleton<UIManager>
 
     private void Start()
     {
+        m_aspectRatioBars.localScale = new Vector3(1,Mathf.Clamp(m_aspectRatioBars.localScale.y, 0.6f, 1),1);
         m_aspectRatioBars.gameObject.SetActive(true);
         foreach (GameObject obj in m_uIObjects)
         {
