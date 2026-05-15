715ef89f3fd5ed02d7e638261c4822e2cb141ce8
JoaquinLam <lam.joaquin021125@gmail.com>
2026-05-15 13:46:05 -0300

    mini scene

--- Assets/_Game/Scripts/UI/UIManager.cs
+++ Assets/_Game/Scripts/UI/UIManager.cs
@@ -10,14 +10,18 @@ public class UIManager : Singleton<UIManager>
     [InfoBox("NOTE: Make sure that the bars' scale is not below 0.6")]
     [SerializeField] private Transform m_aspectRatioBars;
 
-    public float AspectRatioOffset(float max = 540f) =>
-        Mathf.Lerp(max, 0, Mathf.Clamp(m_aspectRatioBars.localScale.y, 0.6f, 1));
+    public float AspectRatioOffset(float max = 540f)
+    {
+        return !m_aspectRatioBars ? 0f : Mathf.Lerp(max, 0, Mathf.Clamp(m_aspectRatioBars.localScale.y, 0.6f, 1));
+    }
 
     public float AspectRatioScale(float min = 0f) =>
         Mathf.Lerp(min, 1, Mathf.Clamp(m_aspectRatioBars.localScale.y, 0.6f, 1));
 
     private void Start()
     {
+        
+        if(!m_aspectRatioBars) return;
         m_aspectRatioBars.localScale = new Vector3(1,Mathf.Clamp(m_aspectRatioBars.localScale.y, 0.6f, 1),1);
         m_aspectRatioBars.gameObject.SetActive(true);
         foreach (GameObject obj in m_uIObjects)
