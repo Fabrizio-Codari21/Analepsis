4ea3c13e12f04ade1a5fa104947fd69bb22dca57
JoaquinLam <lam.joaquin021125@gmail.com>
2026-06-21 21:54:24 -0300

    marg

--- Assets/Editor/Dialogue/DialogueGraphNode.cs
+++ Assets/Editor/Dialogue/DialogueGraphNode.cs
@@ -1,8 +1,14 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
+using EditorUtility = UnityEditor.ProBuilder.EditorUtility;
+
 
 public sealed class DialogueGraphNode : Node
 {
@@ -10,6 +16,10 @@ public sealed class DialogueGraphNode : Node
     public Port InputPort;
     public Port OutputPort;
     private DialogueGraphView _graphView;
+    private AltDialogueSearchWindow _searchWindowProvider;
+    private VisualElement altDialogueContainer;
+    public Foldout nodeFoldOut;
+    public Foldout altDialogueFoldOut;
     public DialogueGraphNode(DialogueNode nodeData, DialogueGraphView graphView)
     {
         if (!nodeData.isRootNode)
@@ -50,6 +60,12 @@ public sealed class DialogueGraphNode : Node
 
         extensionContainer.Add(tagField);
 
+        nodeFoldOut = new Foldout()
+        {
+            text = "Custom Details",
+            value = false,
+        };
+
         Foldout emotionFoldOut = new Foldout()
         {
             text = "How should they emote?",
@@ -77,7 +93,7 @@ public sealed class DialogueGraphNode : Node
         });
 
         emotionFoldOut.Add(emotionField);
-        extensionContainer.Add(emotionFoldOut);
+        nodeFoldOut.Add(emotionFoldOut);
 
         Foldout reactionFoldOut = new Foldout()
         {
@@ -109,37 +125,43 @@ public sealed class DialogueGraphNode : Node
         });
 
         reactionFoldOut.Add(reactionField);
-        extensionContainer.Add(reactionFoldOut);
-
-     
+        nodeFoldOut.Add(reactionFoldOut);
+        
         Foldout proofFoldOut = new Foldout()
         {
             text = "What can it prove?",
             value = false,
         };
-
         
         EnumFlagsField proofField = new EnumFlagsField("What can it prove?", nodeData.doesItProveAnything);
         
+      
+
         proofField.RegisterValueChangedCallback(evt =>
         {
             NodeData.doesItProveAnything = (Whodunnit)evt.newValue;
         });
 
         proofFoldOut.Add(proofField);
-        extensionContainer.Add(proofFoldOut);
+        nodeFoldOut.Add(proofFoldOut);
 
-        Toggle isKeyField = new Toggle("Is this a Key?")
+        Foldout isKeyFoldOut = new Foldout()
         {
-            value = nodeData.isKey
+            text = "Is this a Key?",
+            value = false,
+        };
+
+        Toggle isKeyField = new Toggle("Is Key")
+        {
+            value = false,
         };
         isKeyField.RegisterValueChangedCallback(evt =>
         {
             NodeData.isKey = evt.newValue;
         });
-        extensionContainer.Add(isKeyField);
 
-
+        isKeyFoldOut.Add(isKeyField);
+        nodeFoldOut.Add(isKeyFoldOut);
 
         if (!nodeData.isRootNode)
         {
@@ -153,6 +175,20 @@ public sealed class DialogueGraphNode : Node
             InputPort.portColor = Color.cyan;
             inputContainer.Add(InputPort);
         }
+        else
+        {
+            // Para evitar complicaciones, hice que los dialogos alternativos solo se puedan asignar
+            // al nodo inicial por ahora.
+            altDialogueFoldOut = new Foldout()
+            {
+                text = "Could this dialogue change?",
+                value = false,
+            };
+            GenerateAltDialogueUI();
+        }
+
+        extensionContainer.Add(nodeFoldOut);
+
         OutputPort = InstantiatePort(
             Orientation.Horizontal,
             Direction.Output,
@@ -186,7 +222,232 @@ public sealed class DialogueGraphNode : Node
         RefreshExpandedState();
         RefreshPorts();
     }
-    
+
+    public void GenerateAltDialogueUI()
+    {
+        altDialogueContainer = new VisualElement
+        {
+            style =
+            {
+                flexDirection = FlexDirection.Column,
+                backgroundColor = new Color(.2f, 0f, 0.8f, 0.3f),
+                paddingLeft = 12,
+                paddingTop = 12,
+                paddingRight = 12,
+                paddingBottom = 10,
+                marginRight = 20,
+            }
+        };
+
+        //SerializedObject pathClass = new SerializedObject(nodeData.altDialoguePaths);
+        //SerializedProperty paths = pathClass.FindProperty("Paths");
+
+        //PropertyField altDialogueField = new PropertyField(paths)
+        //{
+        //    name = "Add alternative dialogues",
+        //};
+        //altDialogueField.Bind(pathClass);
+
+        //if(NodeData.altDialoguePath is AltDialoguePath path) // para volver rapido a que sea solo con un altDialogue
+        foreach (AltDialoguePath path in NodeData.altDialoguePaths)
+        {
+            Func<DialogueNode, string> formatLabel = n => {
+                if (n == null) return "Select a Node";
+                string preview = string.IsNullOrEmpty(n.dialogueText) ? "Empty" : n.dialogueText;
+                if (preview.Length > 50) preview = preview[..50] + "...";
+                return $"[{n.tag}] {preview}";
+            };
+            var initialChoices = _graphView.CurrentDialogue?.allNodes ?? new List<DialogueNode>();
+
+            PopupField<DialogueNode> condNodeField =
+            new PopupField<DialogueNode>("If we've reached this point...",
+                initialChoices,
+                0,
+                formatLabel,
+                formatLabel
+            )
+            { value = path.condition };
+            condNodeField.RegisterValueChangedCallback(evt =>
+            {
+                path.condition = evt.newValue;
+            });
+
+            PopupField<DialogueNode> skipToField =
+            new PopupField<DialogueNode>("... we skip to this point...",
+                initialChoices,
+                0,
+                formatLabel,
+                formatLabel
+            )
+            { value = path.skipTo };
+            skipToField.RegisterValueChangedCallback(evt =>
+            {
+                path.skipTo = evt.newValue;
+            });
+
+            // esto despues tiene que cambiarse
+            //ObjectField dialogueRefField = new ObjectField("Select dialogue (placeholder)")
+            //{
+            //    objectType = typeof(Dialogue),
+            //    value = _graphView.CurrentDialogue,
+            //};
+            //dialogueRefField.RegisterValueChangedCallback(evt =>
+            //{
+            //    path.dialogueReference = (Dialogue)evt.newValue;
+            //    UpdatePopupOptions();
+            //    EditorUtility.SetDirty(Selection.activeObject);
+            //});
+
+            TextField altDialogueField = new TextField("... and have them say:")
+            {
+                multiline = true,
+                value = path.altDialogue,
+                style = { paddingBottom = 10, }
+            };
+            altDialogueField.RegisterValueChangedCallback(evt =>
+            {
+                path.altDialogue = evt.newValue;
+            });
+
+
+            void UpdatePopupOptions()
+            {
+                if (_graphView.CurrentDialogue != null
+                    && _graphView.CurrentDialogue.allNodes != null)
+                {
+                    var nodes = _graphView.CurrentDialogue.allNodes;
+
+                    condNodeField.choices = nodes;
+                    skipToField.choices = nodes;
+
+                    if (path.condition != null && nodes.Contains(path.condition))
+                        condNodeField.value = path.condition;
+                    else if (nodes.Count > 0) condNodeField.index = 0;
+
+                    if (path.skipTo != null && nodes.Contains(path.skipTo))
+                        skipToField.value = path.skipTo;
+                    else if (nodes.Count > 0) skipToField.index = 0;
+                }
+                else
+                {
+                    condNodeField.choices = new List<DialogueNode>();
+                    condNodeField.value = null;
+
+                    skipToField.choices = new List<DialogueNode>();
+                    skipToField.value = null;
+                }
+            }
+
+            VisualElement column = new VisualElement { style = { flexDirection = FlexDirection.Column, marginBottom = 5 } };
+
+            VisualElement header = new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Column,
+                    paddingBottom = 25,
+                }
+            };
+
+            Label mainLabel = new Label("<b>(Note: if there are multiple dialogues, higher index = higher priority.)</b>")
+            {
+                style = { paddingBottom = 25, fontSize = 13 }
+            };
+            Label elementLabel = new Label($"<b>Dialogue {NodeData.altDialoguePaths.IndexOf(path)}</b>")
+            {
+                style = { paddingBottom = 10, fontSize = 10 }
+            };
+
+            VisualElement buttonRow = new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    alignItems = Align.Center,
+                } 
+            };
+
+            var index = NodeData.altDialoguePaths.IndexOf(path);
+            Button moveUpBtn = new Button(() => {
+                NodeData.altDialoguePaths.TrySwap(index, index - 1, out var exc);
+                GenerateAltDialogueUI();
+                EditorUtility.SetDirty(Selection.activeObject);
+            })
+            { text = "<<< Lower Priority"};
+            Button moveDownBtn = new Button(() => {
+                NodeData.altDialoguePaths.TrySwap(index, index + 1, out var exc);
+                GenerateAltDialogueUI();
+                EditorUtility.SetDirty(Selection.activeObject);
+            })
+            { text = "Increase Priority >>>"};
+            //Button topBtn = new Button(() => {
+            //    var index = NodeData.altDialoguePaths.IndexOf(path);
+            //    NodeData.altDialoguePaths.TrySwap(index, 0, out var exc);
+            //    GenerateAltDialogueUI();
+            //    EditorUtility.SetDirty(Selection.activeObject);
+            //})
+            //{ text = "MIN Priority" };
+            //Button bottomBtn = new Button(() => {
+            //    var index = NodeData.altDialoguePaths.IndexOf(path);
+            //    NodeData.altDialoguePaths.TrySwap(index, NodeData.altDialoguePaths.Count - 1, out var exc);
+            //    GenerateAltDialogueUI();
+            //    EditorUtility.SetDirty(Selection.activeObject);
+            //})
+            //{ text = "MAX Priority" };
+
+
+            Button removeBtn = new Button(() => {
+                NodeData.altDialoguePaths.Remove(path);
+                GenerateAltDialogueUI();
+                EditorUtility.SetDirty(Selection.activeObject);
+            })
+            { text = "X" };
+
+            if(NodeData.altDialoguePaths.First() != path) column.Add(header);
+            else if (NodeData.altDialoguePaths.Count > 1) column.Add(mainLabel);
+
+            //column.Add(dialogueRefField);
+            column.Add(elementLabel);
+            column.Add(condNodeField);
+            column.Add(skipToField);
+            column.Add(altDialogueField);
+
+            if (NodeData.altDialoguePaths.Count > 1)
+            {
+                if(NodeData.altDialoguePaths.First() != path)
+                {
+                    //if(NodeData.altDialoguePaths.Count > 2) buttonRow.Add(topBtn);
+                    buttonRow.Add(moveUpBtn);
+                }
+                if (NodeData.altDialoguePaths.Last() != path)
+                {
+                    buttonRow.Add(moveDownBtn);
+                    //if (NodeData.altDialoguePaths.Count > 2) buttonRow.Add(bottomBtn);
+                }
+            }
+            column.Add(buttonRow);
+
+            column.Add(removeBtn);
+
+            altDialogueContainer.Add(column);
+        }
+
+
+        Button addDialogueButton = new Button(AddAltDialogue)
+        { text = "+ Add Alt Dialogue", style = { marginRight = 25, } };
+        altDialogueFoldOut.Clear();
+        altDialogueFoldOut.Add(altDialogueContainer);
+        altDialogueFoldOut.Add(addDialogueButton);
+        nodeFoldOut.Add(altDialogueFoldOut);
+    }
+
+    private void AddAltDialogue()
+    {
+        Vector2 mousePos = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+        _graphView.OpenAltDialogueSearchWindow(this, mousePos);
+        //extensionContainer.GetFirstOfType<Foldout>().text = $"Conditions ({ResponseData.m_conditions.Count})";
+    }
+
     public override void SetPosition(Rect newPos)
     {
         base.SetPosition(newPos);
