fdb71ef0f4705d44c8cdaed2cfe01c7f8a7f6d63
JoaquinLam <lam.joaquin021125@gmail.com>
2026-06-21 21:43:19 -0300

    marg

--- Assets/_Game/Scripts/Dialogue/DialogueNode.cs
+++ Assets/_Game/Scripts/Dialogue/DialogueNode.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
+
 // Lo que pueden decir los NPC y como podemos responder a eso.
 [System.Serializable]
 public class DialogueNode : INode,IClue
@@ -25,7 +27,43 @@ public class DialogueNode : INode,IClue
         get => _previousResponse;
         set => _previousResponse = value;
     }
-    
+    public DialogueNode SelectAltDialogue()
+
+    {
+
+        if (altDialoguePaths.All(x => x.condition == null) 
+
+            //|| altDialoguePaths.All(x => x.dialogueReference == null)
+
+            || altDialoguePaths.All(x => !DialogueManager.Instance.CheckDialogue(x.condition.guid))) 
+
+            return this;
+
+
+
+        // Por organizacion, elijo el ultimo de los dialogos que hayas creado en el graph
+
+        // (habria que organizarse creandolos de menos a mas prioritario).
+
+        var path = altDialoguePaths.Last(x => DialogueManager.Instance.CheckDialogue(x.condition.guid));
+
+
+
+        var newNode = new DialogueNode();
+
+        newNode.dialogueText = path.altDialogue;
+
+        newNode.responses = path.skipTo.responses;
+
+        newNode.characterEmotion = path.skipTo.characterEmotion; 
+
+        newNode.characterReaction = path.skipTo.characterReaction;
+
+
+
+        return newNode;
+
+    }[SerializeReference] public List<AltDialoguePath> altDialoguePaths = new List<AltDialoguePath>();
 }
 
 public interface INode {}
