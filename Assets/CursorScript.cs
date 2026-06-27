8b87d430a947739f4fb6c0a1bd90aec757b59a6a
Fabro <fabrizio.codari@davinci.edu.ar>
2026-06-27 02:58:49 -0300

    skewed vector para cursor

--- Assets/CursorScript.cs
+++ Assets/CursorScript.cs
@@ -5,6 +5,7 @@ public class CursorScript : MonoBehaviour
 {
     public Texture2D cursorArrow;
     public Texture2D cursorArrowUpdate;
+    public Vector2 skewedVector = new Vector2(0, 1);
     private Camera _cam;
     [SerializeField]
     private AudioClip _clickClip;
@@ -21,14 +22,14 @@ public class CursorScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             AudioSource.PlayClipAtPoint(_clickClip, _cam.transform.position);
-            //Cursor.SetCursor(cursorArrowUpdate, Vector2.zero, CursorMode.ForceSoftware);
+            Cursor.SetCursor(cursorArrowUpdate, skewedVector, CursorMode.ForceSoftware);
         }
-        /*
+        
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            AudioSource.PlayClipAtPoint(_unclickClip, _cam.transform.position);
+            //AudioSource.PlayClipAtPoint(_unclickClip, _cam.transform.position);
             Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
         }
-            */
+            
     }
 }
